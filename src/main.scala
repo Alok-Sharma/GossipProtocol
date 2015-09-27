980302b81aff426907723b4311f766ba5d41410c
Alok-Sharma <alok.sharma127@gmail.com>
2015-09-26 23:14:22 -0400

    Started basic actor classes

--- src/main.scala
+++ src/main.scala
@@ -1,8 +1,27 @@
-
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.event.Logging
 
 /**
  * @author Alok
  */
-object main {
-  
+class GossipNode extends Actor {
+  val log = Logging(context.system, this)
+  def receive = {
+    case "hello" => println("hello back at you")
+    case _       => println("huh?")
+  }
+}
+
+object Main {
+  def main(args: Array[String]) : Unit = {
+    var numOfNodes = args(0).toInt
+    val system = ActorSystem("HelloSystem")
+    val master = system.actorOf(Props[GossipNode], name = "master")
+    
+    for ( i <- 0 to numOfNodes) {
+      val gossipNode = system.actorOf(Props[GossipNode], name = "gossipNode" + i)   
+    }
+  }
 }
