1585d0c4b90538fb5408998f85b8f892b41b03aa
Alok-Sharma <alok.sharma127@gmail.com>
2015-09-30 20:41:23 -0400

    stuff

--- src/main.scala
+++ src/main.scala
@@ -2,26 +2,58 @@ import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
 import akka.event.Logging
+import akka.actor.ActorRef
+import scala.util.Random
+
+case object rumour
+case object execute
+case object nextNeighbour
 
 /**
  * @author Alok
  */
-class GossipNode extends Actor {
-  val log = Logging(context.system, this)
-  def receive = {
-    case "hello" => println("hello back at you")
-    case _       => println("huh?")
-  }
+class GossipNode(masterNode : MasterNode) extends Actor {
+	val log = Logging(context.system, this)
+
+			def receive = {
+			case `rumour` => {
+				println(self.path.name + " received rumour.")
+        // increment counter
+        //send to a neighbour
+			}
+	}
+}
+
+class MasterNode(numberOfNodes : Int) extends Actor {
+  val system = ActorSystem("HelloSystem")
+  var nodesArray = new Array[ActorRef](numberOfNodes)
+  
+  //Topology for Full Network and for Line.
+	for ( i <- 0 to numberOfNodes - 1) {
+		var gossipNode = system.actorOf(Props(new GossipNode(this)), name = "gossipNode" + i)
+		// add nodes to an array
+		nodesArray(i) = gossipNode
+	}
+
+	//pick one random actor and send rumour.
+	val randomActor = Random.shuffle(nodesArray.toList).head
+	def receive = {
+    case `execute` => {
+      randomActor ! rumour
+		}
+    
+    case `nextNeighbour` => {
+      //fetch a random neighbour from the topology and send the rumour.
+    }
+	}
 }
 
 object Main {
-  def main(args: Array[String]) : Unit = {
-    var numOfNodes = args(0).toInt
-    val system = ActorSystem("HelloSystem")
-    val master = system.actorOf(Props[GossipNode], name = "master")
-    
-    for ( i <- 0 to numOfNodes) {
-      val gossipNode = system.actorOf(Props[GossipNode], name = "gossipNode" + i)   
-    }
-  }
+	def main(args: Array[String]) : Unit = {
+			var numOfNodes = args(0).toInt
+			val system = ActorSystem("HelloSystem")
+			var masterNode = system.actorOf(Props(new MasterNode(numOfNodes)), name = "master")
+      
+      masterNode ! execute
+	}
 }
