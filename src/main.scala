cd3120c3971d3fa265bcf6519447fbc2a95cc896
Nikita Dagar <ndagar@ufl.edu>
2015-10-02 17:02:00 -0400

    Build topology function for line and full

--- src/main.scala
+++ src/main.scala
@@ -33,15 +33,11 @@ class GossipNode extends Actor {
 class MasterNode(numberOfNodes : Int) extends Actor {
 	var nodesArray = new Array[ActorRef](numberOfNodes)
 	val system = ActorSystem("HelloSystem")
-    var topology = "line"
-    
-	//Topology for Full Network and for Line.
-	for ( i <- 0 to numberOfNodes - 1) {
-		var gossipNode = system.actorOf(Props[GossipNode], name = "gossipNode" + i)
-		// add nodes to an array
-		nodesArray(i) = gossipNode
-	}
+  var topology = "line"
+	
+  buildTopology()
 
+  println("inside master")
 	def receive = {
 		case `execute` => {
             //pick one random actor and start rumor.
@@ -55,6 +51,18 @@ class MasterNode(numberOfNodes : Int) extends Actor {
             randomNeighbor ! rumour
 		}
 	}
+    
+  
+	  def buildTopology() : Unit = {
+			  if(topology.equals("line")){
+				    for ( i <- 0 to numberOfNodes - 1) {
+					      var gossipNode = system.actorOf(Props[GossipNode], name = "gossipNode" + i)
+							  // add nodes to an array
+							  nodesArray(i) = gossipNode
+				    }
+			  }
+        //add more for 3D grids
+	  }
     
     def fetchNeighbour(node : ActorRef) : ActorRef = {
         if(topology.equals("line")) {
